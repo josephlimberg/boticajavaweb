e2247dadab2ae028bbf696b82005e139bf01ec9e
Alejandro15927 <alejandroparedeseguia@gmail.com>
2026-06-19 19:56:30 -0500

    Agregando modulo de reportes

--- src/main/java/com/java/boticaintegrador/repository/VentaRepository.java
+++ src/main/java/com/java/boticaintegrador/repository/VentaRepository.java
@@ -1,14 +1,18 @@
 package com.java.boticaintegrador.repository;
 
 import com.java.boticaintegrador.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Long> {
     List<Venta> findByFechaVenta(LocalDate fechaVenta);
+    
+    @Query("SELECT v FROM Venta v WHERE v.fechaVenta BETWEEN :inicio AND :fin")
+    List<Venta> findByFechaVentaBetween(@Param("inicio") LocalDate inicio, @Param("fin") LocalDate fin);
 }
